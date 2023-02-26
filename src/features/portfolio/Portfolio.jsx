d126982920df308bd9cfe61bef75b449c8e52184
Samuel Njenga <snjenga704@gmail.com>
2023-02-26 12:19:27 +0300

    feat: add number Avatars to the Portfolio component

--- src/features/portfolio/Portfolio.jsx
+++ src/features/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import LinkIcon from "@mui/icons-material/Link";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
 
 import Awards from "../awards/Awards";
 
@@ -9,63 +11,90 @@ const Portfolio = () => {
   return (
     <div className="portfolio-main">
       <h4 className="portfolio-title">
-        Take a look at some of the projects am working on currently
+        Take a look at some of the projects am working on currently:
       </h4>
       <div id="portfolio" className="margin-right">
-        <div class="container-fluid p-0">
-          <div class="row g-0">
-            <div class="col-lg-4 col-sm-6">
+        <div className="container-fluid p-0">
+          <div className="row g-0">
+            <div className="col-lg-4 col-sm-6 img-div">
+              <span className="number-text">
+                <Avatar
+                  sx={{ width: 24, height: 24, bgcolor: deepOrange[500] }}
+                >
+                  1
+                </Avatar>
+              </span>
               <a
-                class="portfolio-box"
+                className="portfolio-box"
                 href="https://github.com/SamuelNjenga/restaurant_frontend"
                 title="Restaurant App"
               >
                 <img
-                  class="img-fluid"
+                  className="img-fluid"
                   src="/src/assets/thumbnails/restaurant_caption.jpg"
                   alt="..."
                 />
-                <div class="portfolio-box-caption">
-                  <div class="project-name portfolio-title">RESTAURANT APP</div>
-                  <div class="project-category text-white-50 center-icon">
+                <div className="portfolio-box-caption">
+                  <div className="project-name portfolio-title">
+                    RESTAURANT APP
+                  </div>
+                  <div className="project-category text-white-50 center-icon">
                     App Link <LinkIcon />
                   </div>
                 </div>
               </a>
             </div>
-            <div class="col-lg-4 col-sm-6">
+            <div className="col-lg-4 col-sm-6 img-div">
+              <span className="number-text">
+                <Avatar
+                  sx={{ width: 24, height: 24, bgcolor: deepOrange[500] }}
+                >
+                  2
+                </Avatar>
+              </span>
               <a
-                class="portfolio-box"
+                className="portfolio-box"
                 href="https://github.com/SamuelNjenga/messaging-app"
-                title="Messaging"
+                title="Messaging App"
               >
                 <img
-                  class="img-fluid"
+                  className="img-fluid"
                   src="/src/assets/thumbnails/messaging_caption.jpg"
                   alt="..."
                 />
-                <div class="portfolio-box-caption">
-                  <div class="project-name portfolio-title">MESSAGING APP</div>
-                  <div class="project-category text-white-50 center-icon">
+                <div className="portfolio-box-caption">
+                  <div className="project-name portfolio-title">
+                    MESSAGING APP
+                  </div>
+                  <div className="project-category text-white-50 center-icon">
                     App Link <LinkIcon />
                   </div>
                 </div>
               </a>
             </div>
-            <div class="col-lg-4 col-sm-6">
+            <div className="col-lg-4 col-sm-6 img-div">
+              <span className="number-text">
+                <Avatar
+                  sx={{ width: 24, height: 24, bgcolor: deepOrange[500] }}
+                >
+                  3
+                </Avatar>
+              </span>
               <a
-                class="portfolio-box"
+                className="portfolio-box"
                 href="https://github.com/SamuelNjenga/workout-admin"
                 title="Workout App"
               >
                 <img
-                  class="img-fluid"
+                  className="img-fluid"
                   src="/src/assets/thumbnails/workout_caption.jpg"
                   alt="..."
                 />
-                <div class="portfolio-box-caption">
-                  <div class="project-name portfolio-title">WORKOUT APP</div>
-                  <div class="project-category text-white-50 center-icon">
+                <div className="portfolio-box-caption">
+                  <div className="project-name portfolio-title">
+                    WORKOUT APP
+                  </div>
+                  <div className="project-category text-white-50 center-icon">
                     App Link <LinkIcon />
                   </div>
                 </div>
