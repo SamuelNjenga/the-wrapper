8f4908909d80b93d24109b684786f422d1e00570
Samuel Njenga <snjenga704@gmail.com>
2023-02-26 11:45:39 +0300

    feat: modify Awards component

--- src/features/awards/Awards.jsx
+++ src/features/awards/Awards.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import "./Awards.css";
 const Awards = () => {
   return (
     <div>
-      <h4> Awards & Certifications</h4>
+      <h4 className="awards-title"> Awards & Certifications:</h4>
+      <div className="awards-text">
+        1. Courses on Frontend Tools and Technologies from Udemy.
+      </div>
+      <div className="awards-text">
+        2. Courses on Backend Tools and Technologies from Simplilearn.
+      </div>
+      <div className="awards-text">
+        3. Online Bootcamp on Database Tools and Technologies from Simplilearn.
+      </div>
+      <div className="awards-text">
+        4. Courses on Code Versioning and Agile Tools and Technologies from
+        Coursera.
+      </div>
+      <h4 className="awards-title">
+        Other Challenges I engage in to keep myself grounded and up to par with tech:
+      </h4>
+      <div className="awards-text">1. HackerRank coding challenges.</div>
+      <div className="awards-text">2. LeetCode coding challenges.</div>
     </div>
   );
 };
